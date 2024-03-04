5a328a73d3eb6b4efccab9904d6609dc29bb26e4
sksalmankhan <158127216+sksalmankhan@users.noreply.github.com>
2024-03-05 00:43:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923068320892', 'GURU', true],
+  ['6281255369012', 'SK', true], 
   [''] 
 ] //Number of owners
 
